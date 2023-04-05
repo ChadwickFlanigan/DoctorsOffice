22122878639cf619c7c32300fd6b1407d69f5512
Leemarosi Tamilarasan <lt00044@my.westga.edu>
2023-04-05 01:21:32 -0500

    Tests and PatientVisit model class added

---
+++ CS6232_G2/Model/PatientVisit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6232_G2.Model
+{
+    public class PatientVisit
+    {
+        public decimal Height { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public int Systolic { get; set; }
+
+        public int Diastolic { get; set; }
+
+        public decimal Temperature { get; set; }
+
+        public int Pulse { get; set; }
+
+        public string Symptoms { get; set; }
+
+        public string InitialDiagnosis { get; set; }
+
+        public string FinalDiagnosis { get; set; }
+    }
+}
--- CS6232_G2/Program.cs
+++ CS6232_G2/Program.cs
@@ -14,7 +14,8 @@ namespace CS6232_G2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+           // Application.Run(new LoginForm());
+            Application.Run(new RoutineCheckup());
             //Application.Run(new SearchPatientForm());
         }
     }
